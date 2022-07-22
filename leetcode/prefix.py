bd41717c9eec7bb2255db077d26e5c7c52deafda
josevictormoreno <josevictor.admoreno@gmail.com>
2022-07-22 15:40:11 -0300

    python 1198

---
+++ leetcode/prefix.py
@@ -0,0 +1,8 @@
+casos = int(input())
+string = ['']
+letras = 0
+for x in casos:
+    string.add(input())
+
+for 
+if string[0][0] == string[1][0]
---
+++ python/1198.py
@@ -0,0 +1,22 @@
+# Hashmat é um guerreiro cujo seu grupo de soldados se move de um local a outro para lutar contra os seus oponentes. 
+# Antes da luta ele apenas calcula uma coisa. A diferença entre a quantidade de soldados que possui e a quantidade de soldados oponentes. 
+# A partir desta diferença ele decide se vai ou não lutar. Às vezes Hashmat tem mais soldados do que o seu oponente, mas na maioria das vezes não.
+
+# Entrada
+# A entrada contém dois números inteiros em cada linha. Estes dois números denotam respectivamente a quantidade de soldados 
+# do exército de Hashmat e do seu oponente.  Nenhum número de entrada é maior do que  232. A entrada termina com fim de arquivo (EOF).
+
+# Saída
+# Para cada linha de entrada imprima a diferença entre o número de soldados de Hashmat e do seu oponente. 
+# Cada saída deve ser impressa em uma linha separada.
+
+# Exemplo de Entrada	Exemplo de Saída
+# 10 12                     2
+# 10 14                     4
+# 100 200                   100
+
+a, b = input().split(' ')
+a = int(a)
+b = int(b)
+
+print(b-a)
